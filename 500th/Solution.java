fb70d86208854c0d2ec06bff58abbd2ab6cb6daa
Haochuan Yang <u6001301@n111lt15.csitlabs>
2017-06-18 16:07:10 +1000

    500th Keybord row

--- 500th/Solution.java
+++ 500th/Solution.java
@@ -73,9 +73,4 @@ public class Solution {
         for(int i =0;i<result.length;i++)
             System.out.println(result[i]);
     }
-    /*
-    *
-["asdfghjklASDFGHJKLasdfghjklASDFGHJKLzxcvbnmZXCVBNMaew","asdfghjklASDFGHJKLqwertyuiopQWERTYUIOP","zxcvbnmZXCVBNMaewzxcvbnmZXCVBNMaewzxcvbnmZXCVBNMaewzxcvbnmZXCVBNMaew"]
-    *
-    * */
 }
