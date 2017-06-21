86ef48cc87bc1326c7b8f95e4c5721850e837ea0
Haochuan Yang <u6001301@n111lt15.csitlabs>
2017-06-21 13:04:59 +1000

    Longest Uncommon Subsequence II

--- 522th/Solution.java
+++ 522th/Solution.java
@@ -1,5 +1,10 @@
+import java.util.*;
+
+/**
+ * Created by u6001301 on 21/06/17.
+ */
 public class Solution {
-    public int findLUSlength(String[] strs) {
+    public static int findLUSlength(String[] strs) {
         HashMap<String, Integer> store = new HashMap<String, Integer>();
         // for(int i = 0;i< strs.length;i++)
         //     store.put(strs[i],strs[i].length());
@@ -10,52 +15,56 @@ public class Solution {
         //         return (o2.getValue() - o1.getValue());
         //     }
         // });
+        Arrays.sort(strs, new Comparator<String>() {
+            public int compare(String s1, String s2) {
+                return s2.length() - s1.length();
+            }
+        });
         int max =-1;
         int len = strs.length;
         HashMap<String, Boolean> store1 = new HashMap<String,Boolean>();
         for (int i=0;i<len;i++){
             // String key = entry.getKey();
             // System.out.println(key);
+            int count = len-1;
             for (int j=0;j<len;j++){
                 // String key1 = entry1.getKey();
                 //System.out.println(strs[j] + " " +strs[i] + " " + LUS(strs[j],strs[i]));
                 //System.out.println(key + " " + key1);
                 if(j == i)
                     continue;
-                else if(strs[j] == strs[i]){
-                    store1.put(strs[j],true);
-                    continue;
+//                else if(strs[j] == strs[i]){
+//                    store1.put(strs[j],true);
+//                    continue;
+//                }
+//                else if(strs[j].length() > strs[i].length())
+//                    continue;
+                else if (isLUS(strs[i],strs[j]) == true){
+                    //System.out.println(strs[j] + " " +strs[i] + " " + LUS(strs[j],strs[i]));
+                    //int temp = strs[j].length();
+                    //System.out.println(strs[i] + " " + temp);
+                    //if(temp > max && !store1.keySet().contains(strs[j])){
+                    //if(temp > max)
+                    //max = temp;
+                    System.out.println(count);
+                    count --;
                 }
-                else if(strs[j].length() > strs[i].length())
-                    continue;
-                else if (LUS(strs[j],strs[i]) == false){
-                        //System.out.println(strs[j] + " " +strs[i] + " " + LUS(strs[j],strs[i]));
-                        int temp = strs[j].length();
-                        //System.out.println(strs[i] + " " + temp);
-                        if(temp > max && !store1.keySet().contains(strs[j])){
-                        //if(temp > max)
-                            max = temp;
-                            //System.out.println(max);
-                        }
-                    }
-                else if(LUS(strs[j],strs[i]) == true){
-                        max = -1;
-                        break;
-                    }
+                if(count == 0)
+                    return strs[i].length();
             }
         }
         return max;
     }
-    public boolean LUS(String s1, String s2){
+    public static boolean isLUS(String s1, String s2){
         int count = 0;
         //System.out.println(s2.length());
         //System.out.println(s1.length());
         //if(s1.length() > s2.length())
-           // return true;
+        // return true;
         if(s2.length() == 0 || s1.length() == 0)
             return false;
         for(int i = 0;i<s2.length();i++){
-            if(s2.charAt(i) == s1.charAt(count)){
+            if(s1.charAt(count) == s2.charAt(i)){
                 count++;
             }
             if(count == s1.length())
@@ -63,8 +72,8 @@ public class Solution {
         }
         //System.out.println(s2 + " " + count + "  " + s1.length());
         if(count == s1.length())
-            return true;
-        else
             return false;
+        else
+            return true;
     }
 }
