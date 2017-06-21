6e1e10f9f419435b20e0b6c2ca3a240622c51fe4
Haochuan Yang <u6001301@anu.edu.au>
2017-06-21 12:11:17 +1000

    Edited

--- 522th/Solution.java
+++ 522th/Solution.java
@@ -1,36 +1,47 @@
 public class Solution {
     public int findLUSlength(String[] strs) {
         HashMap<String, Integer> store = new HashMap<String, Integer>();
-        for(int i = 0;i< strs.length;i++)
-            store.put(strs[i],strs[i].length());
-        List<Map.Entry<String, Integer>> slist =
-                new ArrayList<Map.Entry<String, Integer>>(store.entrySet());
-        Collections.sort(slist, new Comparator<Map.Entry<String, Integer>>() {
-            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                return (o2.getValue() - o1.getValue());
-            }
-        });
-        int max = 0;
-        for (Map.Entry<String, Integer> entry : slist){
-            String key = entry.getKey();
-            for (Map.Entry<String, Integer> entry1 : slist){
-                String key1 = entry1.getKey();
-                //System.out.println(key1);
-                if(key == key1)
+        // for(int i = 0;i< strs.length;i++)
+        //     store.put(strs[i],strs[i].length());
+        // List<Map.Entry<String, Integer>> slist =
+        //         new ArrayList<Map.Entry<String, Integer>>(store.entrySet());
+        // Collections.sort(slist, new Comparator<Map.Entry<String, Integer>>() {
+        //     public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+        //         return (o2.getValue() - o1.getValue());
+        //     }
+        // });
+        int max =-1;
+        int len = strs.length;
+        HashMap<String, Boolean> store1 = new HashMap<String,Boolean>();
+        for (int i=0;i<len;i++){
+            // String key = entry.getKey();
+            // System.out.println(key);
+            for (int j=0;j<len;j++){
+                // String key1 = entry1.getKey();
+                //System.out.println(strs[j] + " " +strs[i] + " " + LUS(strs[j],strs[i]));
+                //System.out.println(key + " " + key1);
+                if(j == i)
                     continue;
-                else{
-                    if (LUS(key1,key) == false){
-                        int temp = key1.length();
-                        System.out.println(temp);
-                        if(temp > max)
+                else if(strs[j] == strs[i]){
+                    store1.put(strs[j],true);
+                    continue;
+                }
+                else if(strs[j].length() > strs[i].length())
+                    continue;
+                else if (LUS(strs[j],strs[i]) == false){
+                        //System.out.println(strs[j] + " " +strs[i] + " " + LUS(strs[j],strs[i]));
+                        int temp = strs[j].length();
+                        //System.out.println(strs[i] + " " + temp);
+                        if(temp > max && !store1.keySet().contains(strs[j])){
+                        //if(temp > max)
                             max = temp;
+                            //System.out.println(max);
+                        }
                     }
-                    else if(LUS(key1,key) == true){
-                        //System.out.println(key1);
+                else if(LUS(strs[j],strs[i]) == true){
                         max = -1;
                         break;
                     }
-                }
             }
         }
         return max;
@@ -39,6 +50,8 @@ public class Solution {
         int count = 0;
         //System.out.println(s2.length());
         //System.out.println(s1.length());
+        //if(s1.length() > s2.length())
+           // return true;
         if(s2.length() == 0 || s1.length() == 0)
             return false;
         for(int i = 0;i<s2.length();i++){
@@ -48,7 +61,8 @@ public class Solution {
             if(count == s1.length())
                 break;
         }
-        if(count == s2.length())
+        //System.out.println(s2 + " " + count + "  " + s1.length());
+        if(count == s1.length())
             return true;
         else
             return false;
