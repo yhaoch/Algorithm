cfcdca3ae7ddb1851f2e3b58832a5712bb21f977
Haochuan Yang <u6001301@anu.edu.au>
2017-06-25 13:13:08 +1000

    public class Solution {

---
+++ 628th/Solution.java
@@ -0,0 +1,65 @@
+public class Solution {
+    public int maximumProduct(int[] nums) {
+        if(nums.length == 3){
+            return nums[0] * nums[1] * nums[2];
+        }
+        else{
+            int temp = 0;
+            int len = nums.length;
+            // for(int i=len-1;i>=0;i--){
+            //     for(int j=0;j<i;j++){
+            //         if(nums[j+1] < nums[j]){
+            //             temp = nums[j];
+            //             nums[j] = nums[j+1];
+            //             nums[j+1] = temp;
+            //         }
+            //     }
+            // }
+            recsiveSort(nums,0,len-1);
+            // for(int i = 0;i<len;i++){
+            //     System.out.println(nums[i]);
+            // }
+            int product = nums[len-1] * nums[len-2] * nums[len-3];
+            //System.out.println(product);
+            if(product < 0){
+                if(nums[len-1] < 0){
+                    return nums[0] * nums[1] * nums[2];
+                }
+                return product;
+            }
+            else if(product > 0){
+                if(nums[len-3] < 0){
+                    return nums[0] * nums[1] * nums[len-1];
+                }
+                else{
+                    if(nums[len-2]*nums[len-3] < nums[0]*nums[1])
+                        return nums[0]*nums[1]*nums[len-1];
+                }
+                return product;
+            }
+            return nums[len-1] * nums[len-2] * nums[len-3];
+        }
+    }
+    public int RTI(int[] list, int low, int high){
+    int tmp = list[low];
+    while(low < high){
+      while(low < high && list[high] >= tmp){
+        high --;
+      }
+      list[low] = list[high];
+      while(low < high && list[low]<= tmp){
+        low ++;
+      }
+      list[high] = list[low];
+    }
+    list[low] = tmp;
+    return low;
+  }
+  public void recsiveSort(int [] list, int low, int high){
+    if(low < high){
+      int mid = RTI(list, low, high);
+      recsiveSort(list,low, mid-1);
+      recsiveSort(list,mid+1,high);
+    }
+  }
+}
