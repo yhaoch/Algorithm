3ccc05379ee43ed90807026b00b9a4806d6a4d26
Haochuan Yang <u6001301@n112lt08.csitlabs>
2017-07-09 16:03:51 +1000

    K-diff Pairs in an Array

---
+++ 532th/Solution.java
@@ -0,0 +1,45 @@
+public class Solution {
+    public int findPairs(int[] nums, int k) {
+        if(k<0 || nums.length == 0)
+            return 0;
+        HashMap<Integer,Integer> st = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(st.keySet().contains(nums[i]) == true)
+                st.put(nums[i],st.get(nums[i]) + 1);
+            else{
+                st.put(nums[i],0);
+            }
+        }
+        if(k== 0){
+            int count1 = 0;
+            for(Integer key:st.keySet()){
+                //System.out.println(st.get(key));
+                if(st.get(key) >= 1)
+                    count1 ++;
+            }
+            return count1;
+            //System.out.println(key);
+        }
+        // List<Integer> st1 = new ArrayList<>();
+        // for(Integer key:st.keySet()){
+        //     st1.add(key);
+        //     //System.out.println(key);
+        // }
+        // int count = 0;
+        // for(int i=0;i<st1.size()-1;i++){
+        //     // for(int j=i+1;j<st1.size();j++){
+        //     //     if(Math.abs(st1.get(i) - st1.get(j)) == k)
+        //     //         count ++;
+        //     // }
+        // }
+        else{
+            int count = 0;
+            for(Integer key:st.keySet()){
+                //System.out.println(st.get(key));
+                if(st.keySet().contains(key + k) == true)
+                    count ++;
+            }
+            return count;
+        }
+    }
+}
