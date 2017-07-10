24c346cc0a4597a2ecc4b358145bb2b9fc61065e
Haochuan Yang <u6001301@n112lt08.csitlabs>
2017-07-10 17:45:28 +1000

    152th

---
+++ 152th/Solution.java
@@ -0,0 +1,53 @@
+public class Solution {
+    public static int maxProduct(int[] nums) {
+        Stack<Integer> st = new Stack<>();
+            Stack<Integer> res = new Stack<>();
+        int [] dp = new int [nums.length];
+        int max = 0;
+        int Ma = 0;
+        if(nums.length == 0)
+            return 0;
+        else if(nums.length == 1)
+            return nums[0];
+        else{
+            dp[0] = nums[0];
+            max = dp[0];
+            if(max < 0) {
+                st.push(dp[0]);
+                dp[0] = 1;
+            }
+            for(int i=1;i<nums.length;i++){
+                if(nums[i] > 0){
+                    dp[i] = dp[i-1] * nums[i];
+                    max = Math.max(dp[i],max);
+                }
+                else if(nums[i] ==0){
+                    max = Math.max(dp[i],max);
+                    dp[i] = 1;
+                }
+                else{
+                    if(st.isEmpty() != true){
+                        int temp = st.pop();
+                        dp[i] = dp[i-1] * temp * nums[i];
+                        max = Math.max(dp[i],max);
+                        if(res.isEmpty()!=true){
+                            int ma = res.pop();
+                            max = max * ma;
+                            //res.push(max);
+                        }
+                        dp[i] = 1;
+                        st.push(nums[i]);
+                    }
+                    else{
+                        st.push(nums[i]);
+                        dp[i] = 1;
+                        max = Math.max(dp[i],max);
+                        res.push(max);
+                        //dp[i] = 1;
+                    }
+                }
+            }
+        }
+        return max;
+    }
+}
