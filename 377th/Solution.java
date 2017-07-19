09e13a588f79bfba40f1a8fe2d5828da15b1420e
Haochuan Yang <u6001301@anu.edu.au>
2017-07-19 10:19:27 +1000

    Combination Sum IV

---
+++ 377th/Solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+    // public int combinationSum4(int[] nums, int target) {
+    //     if(target == 0)
+    //         return 1;
+    //     else{
+    //         int res = 0;
+    //         for(int i=0;i<nums.length;i++){
+    //             if(target >= nums[i]){
+    //                 res += combinationSum4(nums,target - nums[i]);
+    //             }
+    //         }
+    //         return res;
+    //     }
+    // }
+
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i - nums[j] >= 0) {
+                    dp[i] += dp[i - nums[j]];
+                }
+            }
+        }
+        return dp[target];
+    }
+}
