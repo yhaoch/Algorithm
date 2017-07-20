82b505b134879e37212b4d9ed7d6f12015a47de1
Haochuan Yang <u6001301@anu.edu.au>
2017-07-20 12:05:16 +1000

    Minimum Path Sum

---
+++ 64th/Solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int minPathSum(int[][] grid) {
+        if(grid.length == 0 || grid[0].length == 0)
+            return 0;
+        else{
+            int [][] dp = new int [grid.length][grid[0].length];
+            //boundary 
+            dp[0][0] = grid[0][0];
+            for (int i = 1; i < grid.length; i++) {
+                dp[i][0] = dp[i - 1][0] + grid[i][0];
+            }
+            for (int i = 1; i < grid[0].length; i++) {
+                dp[0][i] = dp[0][i - 1] + grid[0][i];
+            }
+            //int [][] dp = new int [grid.length][grid[0].length];
+            for(int i = 1;i<grid.length;i++){
+                for(int j = 1;j<grid[0].length;j++){
+                    dp[i][j] = Math.min(dp[i-1][j],dp[i][j-1]) + grid[i][j];
+                }
+            }
+            return dp[grid.length-1][grid[0].length-1];
+        }
+    }
+}
